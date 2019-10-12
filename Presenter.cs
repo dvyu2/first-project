8e7e3576700080a4f9ed2280a5366f9338dee53d
Dvin <dvyu@yandex.ru>
2019-10-12 13:53:05 +0500

    Add functionality

---
+++ Presenter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Presenter
+    {
+        int[,] a = new int[10, 10];
+
+        public void Add()
+        {
+            //int[,] a = new int[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    //r num = random.Next();
+                    //a[i, j] = r;
+                    a[i, j] = (i + j) % 10 + 1;
+                }
+            }
+        }
+
+        public void Out()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                    Console.Write(a[i, j] + " ");
+                    Console.WriteLine();
+            }
+        }
+
+        public void Del(int i, int j)
+        {
+            a[i, j] = 0;
+        }
+
+        public void Task()
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                //Console.Write(a[i, 0]);
+                if(a[k, 0] == 0)
+                {
+                    Console.WriteLine("Find Zerro!!! - Remove first string array");
+                    int[,] b = new int[9, 10];
+                    for (int i = 1; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            a[i, j] = b[i-1, j];
+                        }
+                    }
+                    a = b;
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace first_project
+namespace ConsoleApplication1
 {
     class Program
     {
+        static Presenter presenter = new Presenter();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                int caseSwitch;
+                Console.WriteLine("Schoose one:\n\t1. Add\n\t2. Del\n\t3. Out\n\t4. Task");
+                try
+                {
+                    caseSwitch = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    caseSwitch = 0;
+                    Console.WriteLine("Number Only Permitted!");
+                }
+                finally
+                {
+                    //Console.WriteLine("TryFinal!");
+                }
+                switch (caseSwitch)
+                {
+                    case 1:
+                        Console.WriteLine("Adding...");
+                        presenter.Add();
+                        break;
+                    case 2:
+                        Console.WriteLine("Type X Element index to delete");
+                        int x = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Type Y Element index to delete");
+                        int y = Int32.Parse(Console.ReadLine());
+                        presenter.Del(x,y);
+                        break;
+                    case 3:
+                        Console.WriteLine("Printing...");
+                        presenter.Out();
+                        break;
+                    case 4:
+                        Console.WriteLine("Task...");
+                        presenter.Task();
+                        break;
+                    /*default:
+                        Console.WriteLine("Default case");
+                        break;*/
+                }
+            } while (true);
+            //Console.ReadLine();
         }
     }
 }
